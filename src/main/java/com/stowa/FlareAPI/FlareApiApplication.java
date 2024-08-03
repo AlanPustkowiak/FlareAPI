109d83ed4848819bac90482003e8ede489f429e1
AlanPustkowiak <alan.pustkowiak@outlook.com>
2024-08-03 17:14:40 +0200

    init

---
+++ src/main/java/com/stowa/FlareAPI/FlareApiApplication.java
@@ -0,0 +1,13 @@
+package com.stowa.FlareAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stowa/FlareAPI/FlareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stowa.FlareAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
