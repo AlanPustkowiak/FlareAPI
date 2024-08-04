a99240ab6e23db595a3611f3f56322f31a6c6083
AlanPustkowiak <alan.pustkowiak@outlook.com>
2024-08-04 21:37:59 +0200

    mysql instead of mssql, some problems with new versions of spring

---
+++ src/main/java/com/stowa/FlareAPI/model/Employee.java
@@ -0,0 +1,75 @@
+package com.stowa.FlareAPI.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String jobTitle;
+
+    public Employee(String name, String email, String phone, String jobTitle) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.jobTitle = jobTitle;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
+}
