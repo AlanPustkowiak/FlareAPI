604c6755bf6968005f219a017f946bdb5ec3eee6
Alan Pustkowiak <alan.pustkowiak@outlook.com>
2025-03-18 20:58:13 +0100

    Added DepartmentDTO

---
+++ src/main/java/com/stowa/FlareAPI/model/dto/DepartmentDTO.java
@@ -0,0 +1,16 @@
+package com.stowa.FlareAPI.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDTO {
+    private Integer id;
+    private String name;
+    private String description;
+}
